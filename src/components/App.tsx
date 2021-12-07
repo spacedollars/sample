cf6324f21cbdd8578cfad0df8130c9655b27a5da
moonmidas <donkonstante@gmail.com>
2021-12-06 21:43:33 -0600

    Trying to fetch the fuckin Loots

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,5 @@
 import { useAddress } from "../hooks/useConnectedAddress"
-import SendButton from "./SendButton"
 import ConnectButton from "./ConnectButton"
-import ExecuteButton from "./ExecuteButton"
 import MintButton from "./MintButton"
 
 
--- src/components/MintButton.tsx
+++ src/components/MintButton.tsx
@@ -4,37 +4,115 @@ import { MsgExecuteContract } from "@terra-money/terra.js"
 import useConnectedAddress from "../hooks/useConnectedAddress"
 import { useGasPrice } from "../hooks/useGasPrices"
 import useLCDClient from "../hooks/useLCDClient"
+var Bottleneck = require("bottleneck/es5");
+const limiter = new Bottleneck({
+    minTime: 100
+});
 
 const sdollar = "terra182pssdpcahrwt639ankmsjs25pjw3as9tyy46v" // testnet
 const lootFactory = "terra1k8jryjfeqtff3d9tucx0gk2l0h6h360452h989" // testnet
+const cw721Contract = "terra1e5p3yeuyw270mvydkxj3ewjkck8eh33fdj7q3d" // testnet
+
 
 const MintButton = () => {
-  const { post, sign } = useWallet()
+  const { post } = useWallet()
   const address = useConnectedAddress()
   const gasPrices = useGasPrice("uusd")
  
-  const [minting, setMinting] = useState()
+  const [minting, setMinting] = useState("")
 
   // Minting Quantity and Max Minting Quantity per user
   const [mintingQuantity, setMintingQuantity] = useState(0)
   const [maxMintingQuantity, setMaxMintingQuantity] = useState(0)
-
-  // LCD Client
-  const lcdClient = useLCDClient()
-  let contractInfo = {}
-  try {
-    contractInfo = lcdClient.wasm.contractQuery(lootFactory, {
-      "contract_config": {}
-    });
-  } catch (e) {
-      console.log(e)
+  const [txHash, setTxHash] = useState()
+  const [mintingError, setMintingError] = useState("")
+  let url: any;
+  /*
+  {
+    "height": "6887450",
+    "result": {
+      "can_mint_more": true,
+      "amount_minted": 1,
+      "curr_num_tokens": 12,
+      "max_tokens": 30,
+      "limit_per_address": 10
+    }
   }
+  {"wallet_permissions":{"address":"terra1w03mtgc2gkmeqsvqf8a8zw5s2f30wfyduwnmnn"}}
+*/
+  const lcdClient = useLCDClient()
+  useEffect(() => { 
+    const getMintingQuantities = async () => {
+      let walletPermissions: any;
+      try {
+        walletPermissions = await lcdClient.wasm.contractQuery(lootFactory, {
+          "wallet_permissions":{"address":address}
+        });
+        setMaxMintingQuantity(walletPermissions.limit_per_address)
+        setMintingQuantity(walletPermissions.amount_minted)
+      } catch (e) {
+          console.log(e)
+      }
+      console.log(walletPermissions);
+    }
+  
+    getMintingQuantities()
+  }, [])
 
+  // View the loot
+  const [loots, setLoots] = useState([{ 
+    name: null,
+    image: null,
+    attributes: [null],
+  }])
+  useEffect(() => { 
+    let lootsArray: any;
+    const getLootInfo = async (tokens:any) => {
+      try {
+        await tokens.forEach(
+          async (token:any) => {
+            try {
+              const tokenInfo = await limiter.schedule(() => lcdClient.wasm.contractQuery(cw721Contract, {
+                "nft_info": { "token_id": token } 
+              }));
+
+              setLoots([tokenInfo.extension])
+            }
+            catch (e) {
+              console.log(e)
+            }
+          }
+        );
+        return lootsArray;
+        //setLoots([...loots, tokenInfo.extension])
+
+      }
+      catch (e) {
+        console.log(e)
+      }
+
+    }
+    const getLoots = async () => {
+      let tokens: any;
+      try {
+        tokens = await lcdClient.wasm.contractQuery(cw721Contract, {
+          "tokens":{"owner":address}
+        });
+        console.log(tokens.tokens);
+        getLootInfo(tokens.tokens);
+        
+      } catch (e) {
+          console.log(e)
+      }
+    }
+  
+    getLoots()
+  }, [])
+  
+  
   let mintResponse: any;
 
   if (!gasPrices) return null
-
-
 
   const mint = async () => {
     try {
@@ -57,29 +135,60 @@ const MintButton = () => {
       const txOptions = { msgs, gasPrices }
       try { 
         mintResponse = await post(txOptions) 
-        console.log(mintResponse)
-        setMinting(mintResponse)
+        setMinting("Minting Successful!");
+        url = "https://finder.extraterrestrial.money/testnet/tx/" + mintResponse.result.txhash;
+        setTxHash(url)
+        setMintingQuantity(mintingQuantity+1)
       } catch(e) { console.log(e) }
 
  
     } catch (error: any) {
       mintResponse = "Error minting"
-      setMinting(mintResponse)
+      setMintingError(mintResponse)
       console.log(error.name)
     }
   }
+
+
+
+
   return (
-    <div>
+    <>
       <div>
-        <p className="text-center">Can mint: {mintingQuantity}/{maxMintingQuantity}</p>
+        <div>
+          <p className="text-center">Can mint: {mintingQuantity}/{maxMintingQuantity}</p>
+        </div>
+        <div className="text-center">
+          <button className="nes-btn is-primary" onClick={mint}>Mint Weapons Loot</button>
+        </div>
+        {
+          minting !== "" ?
+          <div>
+            <p></p>
+            <p className="text-center">{minting}</p>
+            <p className="text-center"><a target="_blank" rel="noreferrer" href={txHash}>View Transaction</a></p>
+          </div>
+          : 
+          ""
+        }
+        {
+          mintingError !== "" ?
+          <div>
+            <p className="text-center">{mintingError}</p>
+          </div>
+          : 
+          ""
+        }
       </div>
-      <div className="text-center">
-        <button className="nes-btn is-disabled" onClick={mint}>Mint</button>
-      </div>
+
       <div>
-        <span>{minting}</span>
+        <div>
+        <ul>
+          { loots !== null ? loots.map((loot, i) => <li>{loot.name}</li>) : "" }
+        </ul>
+        </div>
       </div>
-    </div>
+    </>
   )
 }
 
